be9946a907110d96dd8402ef46b07830064bb1d0
breiner2000 <breinergcp@estudiantec.cr>
2023-10-20 22:38:06 -0600

    vscode_installer

--- install_apps.sh
+++ install_apps.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
-./scripts/docker_cli_installer.sh 
-docker_cli_res=$? 
+# ./scripts/docker_cli_installer.sh 
+# docker_cli_res=$? 
 
 # ./scripts/docker_desktop_installer.sh
 # docker_desktop_res=$?
 
+./scripts/vscode_installer.sh
+vscode_res=$?
 
-if [ "$docker_cli_res" -eq 0 ]; then
-    echo -e "\e[1;32m Docker instalado correctamente. Prueba con docker run hello-world
+if [ "$vscode_res" -eq 0 ]; then
+    echo -e "\e[1;32m VSCode instalado correctamente.
     \e[0m"
 else
-    echo -e "\e[1;31m Error al instalar Docker.
+    echo -e "\e[1;31m Error al instalar VSCode.
     \e[0m"
 fi
+
+
+# if [ "$docker_cli_res" -eq 0 ]; then
+#     echo -e "\e[1;32m Docker instalado correctamente. Prueba con docker run hello-world
+#     \e[0m"
+# else
+#     echo -e "\e[1;31m Error al instalar Docker.
+#     \e[0m"
+# fi
 
 # if [ "$docker_desktop_res" -eq 0 ]; then
 #     echo -e "\e[1;32m Docker Desktop instalado correctamente.
--- scripts/docker_cli_installer.sh
+++ scripts/docker_cli_installer.sh
@@ -1,41 +1,4 @@
 #!/bin/sh
-# Se puede descargar la última versión desde https://download.docker.com/linux/static/stable/x86_64/
-
-DOCKER_BINARIES="https://download.docker.com/linux/static/stable/x86_64/docker-24.0.6.tgz"
-
-sudo wget $DOCKER_BINARIES -O docker-latest.tgz
-
-sudo tar -xvzf docker-latest.tgz
-
-sudo rm -rf docker-latest.tgz
-
-sudo mv docker/* /usr/bin/
-
-sudo rm -rf docker
-
-# todo: verificar si hay que ejecutar ese comando
-# sudo dockerd &
-
-# Agregar usuario al grupo para ejecutar docker sin sudo
-
-sudo groupadd docker
-
-sudo usermod -aG docker $USER
-
-# Configurar docker para que se inicie con el sistema
-
-wget https://raw.githubusercontent.com/moby/moby/master/contrib/init/systemd/docker.service
-
-wget https://raw.githubusercontent.com/moby/moby/master/contrib/init/systemd/docker.socket
-
-sudo mkdir -p /etc/systemd/system/
-
-sudo mv -f docker.service docker.socket /etc/systemd/system/
-
-sudo systemctl daemon-reload
-
-sudo systemctl enable docker
-
 
 
 
